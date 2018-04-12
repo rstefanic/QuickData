b43502113ff6b58e549306282c5a22d28097e97b
Robert <rstefanic72@gmail.com>
2018-04-12 18:27:23 -0500

    forgot to add types

--- src/QuickData/Types.hs
+++ src/QuickData/Types.hs
@@ -4,29 +4,58 @@ module QuickData.Types where
 
 import Dhall
 import Data.DateTime
+import Data.Text      as T
 
+type Columns = [Column]
 newtype Words = Words [String] deriving (Eq, Show)
 
-data Example = Example { foo :: Integer, bar :: Vector Double }
+data Column = 
+     Column { columnName :: T.Text
+            , columnType :: T.Text
+            , allowNull  :: Bool
+            }
+     deriving (Eq, Show)
+
+data MetaData =
+     MetaData { tableName :: !T.Text
+              , rowCount  :: !Int 
+              }
+     deriving (Eq, Show)
+
+data Table = 
+     Table { metaData :: MetaData
+           , columns  :: Columns       
+           }
+     deriving (Eq, Show)
+
+data Size 
+    = Size Int
+    | Max
+    deriving (Eq, Show)
+
+data SqlType 
+    = SqlBigInt
+    | SqlInt 
+    | SqlSmallInt
+    | SqlTinyInt
+    | SqlBit 
+    | SqlFloat
+    | SqlDate
+    | SqlDateTime
+    | SqlText
+    | SqlNText 
+    | SqlChar Size
+    | SqlNChar Size
+    | SqlVarChar Size
+    | SqlNVarChar Size
+    | SqlBinary Size
+    | SqlVarBinary Size
+    deriving (Eq, Show)
+
+data Example = 
+     Example { foo :: Integer
+             , bar :: Vector Double 
+             }
   deriving (Generic, Show)
 
 instance Interpret Example
-
-data SqlServerTypes 
-    = SSBigInt
-    | SSInt 
-    | SSSmallInt
-    | SSTinyInt
-    | SSBit 
-    | SSFloat
-    | SSDate
-    | SSDateTime
-    | SSChar
-    | SSText
-    | SSVarChar
-    | SSNChar
-    | SSNText
-    | SSNVarChar
-    | SSBinary
-    | SSVarBinary
-    deriving (Eq, Show)
