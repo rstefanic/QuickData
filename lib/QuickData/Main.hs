02e1162cdf03e6c8aaed5fe238fbcb2d64bff04f
Robert <rstefanic72@gmail.com>
2018-05-15 21:13:08 -0500

    Added Opt-Parse to project; added the ability to save to a file

--- lib/QuickData/Main.hs
+++ lib/QuickData/Main.hs
@@ -3,12 +3,51 @@
 module QuickData.Main 
   ( main 
   ) where
+    
+import Data.Semigroup ((<>))
+import qualified Data.Text.IO as DTI (writeFile)
+import Options.Applicative 
+  ( (<**>)
+  , Parser
+  , helper
+  , optional
+  , strOption
+  , long
+  , info
+  , short
+  , metavar
+  , help
+  , execParser
+  , fullDesc
+  , progDesc
+  , header
+  )
 
 import qualified QuickData.Sql   as SQL
 import qualified QuickData.Parse as Parse
 
+newtype Options = Options { outputFile :: Maybe String }
+
+options :: Parser Options
+options = Options <$> 
+       optional 
+     ( strOption
+     $ long "output"
+    <> short 'o'
+    <> metavar "FILE"
+    <> help "Output the statement into a text file." 
+     )
+
 main :: IO ()
 main = do 
-  config <- Parse.getConfig 
-  insertStatement <- SQL.insertValues config 
-  print insertStatement
+    userOpts <- execParser opts
+    config <- Parse.getConfig 
+    insertStatement <- SQL.insertValues config 
+    case outputFile userOpts of 
+      Nothing   -> print insertStatement
+      Just file -> DTI.writeFile file insertStatement
+
+    where opts = info (options <**> helper)
+                   ( fullDesc 
+                  <> progDesc "Generate random data for SQL based on a configuration."
+                  <> header "QuickData -- Generate Quick Data for SQL")
